fc13cacb8d06f6dec97a521a5aa8e728d3fc8ca5
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-06-26 13:46:38 +0200

    Add new test class - PermutationTest

---
+++ src/test/java/PermutationTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermutationTest {
+
+    private Permutation permutation;
+
+    @BeforeEach
+    void setup() {
+        this.permutation = new Permutation();
+    }
+
+
+
+}
