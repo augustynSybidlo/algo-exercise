7dc8096d93832f37118ed834ccb8a63fb5d34935
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-06-26 13:29:38 +0200

    Add previously created algoritm excercise/class FindProductOfIntegers and new class Permutation

---
+++ src/main/java/FindProductOfIntegers.java
@@ -0,0 +1,28 @@
+public class FindProductOfIntegers { // find highest product of two integers
+
+    public int findMaxProductOfTwoIntegers(int[] arr) throws IllegalArgumentException {
+        if (arr.length < 2) throw new IllegalArgumentException();
+
+        int maxOne = 0;
+        int maxTwo = 0;
+
+        int minOne = Integer.MIN_VALUE;
+        int minTwo = Integer.MIN_VALUE;
+
+
+        for (int num : arr) {
+            if (num > maxOne) {
+                maxTwo = maxOne;
+                maxOne = num;
+            } else if (num > maxTwo) {
+                maxTwo = num;
+            } else if (num > minOne) {
+                minTwo = minOne;
+                minOne = num;
+            } else if (num > minTwo) {
+                minTwo = num;
+            }
+        }
+        return Integer.max(minOne * minTwo, maxOne * maxTwo);
+    }
+}
---
+++ src/test/java/FindProductOfIntegersTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindProductOfIntegersTest {
+
+    private FindProductOfIntegers fp;
+
+    @BeforeEach
+    void setup() {
+        this.fp = new FindProductOfIntegers();
+    }
+
+    @Test
+    void testFindMaxProductOfTwoIntegersWhenAllPositive() {
+        int[] arr = {11, 5, 9, 85, 44, 8, 15};
+        int expected = 3740;
+        int actual = fp.findMaxProductOfTwoIntegers(arr);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindMaxProductOfTwoIntegersWhenAllNegative() {
+        int[] arr = {-11, -5, -9, -85, -44, -8, -15};
+        int expected = 40;
+        int actual = fp.findMaxProductOfTwoIntegers(arr);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindMaxProductOfTwoIntegers() {
+        int[] arr = {-11, 5, -9, 85, -44, 8, -15};
+        int expected = 680;
+        int actual = fp.findMaxProductOfTwoIntegers(arr);
+        assertEquals(expected, actual);
+    }
+
+}
