7efdbe50e65f5de67ed485d5eb0dbbdef57b8cae
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-06-26 15:10:56 +0200

    Add new test cases/methods to PairFinderTest class

--- src/test/java/PairFinderTest.java
+++ src/test/java/PairFinderTest.java
@@ -1,5 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PairFinderTest {
+
+    private PairFinder pf;
+
+    @BeforeEach
+    void setup() {
+        this.pf = new PairFinder();
+    }
+
+    @Test
+    void testPairFinderWhenPairPresent() {
+        int[] numbers = {1, 5, 77, 9, 5, 18, 21, 1, 4};
+        int value = 6;
+
+        assertTrue(() -> pf.isPair(numbers, value));
+    }
+
+    @Test
+    void testPairFinderWhenPairNotPresent() {
+        int[] numbers = {1, 5, 77, 9, 5, 18, 21, 1, 4};
+        int value = 100;
+
+        assertFalse(() -> pf.isPair(numbers, value));
+    }
+
+    @Test
+    void testPairFinderWhenSomeNumbersAreNegative() {
+        int[] numbers = {1, 5, 77, 9, 5, -18, 21, 1, 4};
+        int value = 59;
+
+        assertTrue(() -> pf.isPair(numbers, value));
+    }
 
 }
