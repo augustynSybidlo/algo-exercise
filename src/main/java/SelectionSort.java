258e81a258172e3150ec06d72129281973e78f8d
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-06-26 16:14:47 +0200

    Add sort method's body - passed test case

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -5,7 +5,16 @@ Selection sort algorithm for array on numbers.
 public class SelectionSort {
 
    public String sort(int[] numbers) {
-        return toString(numbers);
+       for (int x = 0; x < numbers.length; x++) {
+           for (int y = x; y < numbers.length; y++) {
+               if (numbers[x] > numbers[y]) {
+                   int temp = numbers[x];
+                   numbers[x] = numbers[y];
+                   numbers[y] = temp;
+               }
+           }
+       }
+       return toString(numbers);
    }
 
    private String toString(int[] array) {
