82f1182d0718f14c095cb0b8416e6777ce48fd64
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-06-26 13:44:16 +0200

    Add method which checks if one string is permutation of another

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -4,6 +4,21 @@ if two strings are permutations of each other.
  */
 
 public class Permutation {
-    
 
+    public boolean isPermutation(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() != str2.length()) return false;
+        else if (!str1.equals(str2)) {
+            int[] buffer = new int[256];
+
+            for (int x = 0; x < str1.length(); x++) {
+                buffer[str1.charAt(x)]++;
+                buffer[str2.charAt(x)]--;
+            }
+
+            for (int count : buffer) {
+                if (count != 0) return false;
+            }
+        }
+        return true;
+    }
 }
