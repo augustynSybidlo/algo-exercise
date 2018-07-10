b93429172d1f28960857d92a14e88b154c7c89a9
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-10 10:49:14 +0200

    Add setup for test and test of sorting method

--- src/test/java/sorting/algorithms/QuickSortTest.java
+++ src/test/java/sorting/algorithms/QuickSortTest.java
@@ -1,5 +1,28 @@
+package sorting.algorithms;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuickSortTest {
+
+    private QuickSort quickSort;
+
+    @BeforeEach
+    void setup() {
+        this.quickSort = new QuickSort();
+    }
+
+    @Test
+    void testQuickSortIfOnlyPositiveNums() {
+        int[] arr = {5,6,1,2,8,9,0,3,4,5,1,1};
+        int low = 0;
+        int high = arr.length - 1;
+        quickSort.sort(arr,low, high);
+        String actual = quickSort.toString(arr);
+        String expected = "011123455689";
+        assertEquals(expected, actual);
+    }
 
 }
