fbb1cecbbf762383fc840646ac9d1c596a386d62
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-10 10:47:27 +0200

    Add method toString and signature of method sort

--- src/main/java/sorting/algorithms/QuickSort.java
+++ src/main/java/sorting/algorithms/QuickSort.java
@@ -1,4 +1,18 @@
 package sorting.algorithms;
 
 public class QuickSort {
+
+    public static void sort(int[]arr, int low, int high) {
+
+    }
+
+    public String toString(int[]arr) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : arr) {
+            sb.append(num);
+        }
+
+        return sb.toString();
+    }
 }
